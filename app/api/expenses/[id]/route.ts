b33297d45f66daef8a3845d5263870f7707f422b
Vidhu Arora <vidhu18102@gmail.com>
2025-04-14 17:34:17 +0530

    testing explicit options method handler

--- app/api/expenses/[id]/route.ts
+++ app/api/expenses/[id]/route.ts
@@ -129,15 +129,15 @@ export async function DELETE(
     }
 }
 
-export async function OPTIONS() {
-    return new NextResponse(null, {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Origin": "https://expense-tracker-frontend-theta-one.vercel.app",
-        "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-        "Access-Control-Allow-Headers":
-          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-      },
-    });
-  }
+// export async function OPTIONS() {
+//     return new NextResponse(null, {
+//       status: 204,
+//       headers: {
+//         "Access-Control-Allow-Credentials": "true",
+//         "Access-Control-Allow-Origin": "https://expense-tracker-frontend-theta-one.vercel.app",
+//         "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+//         "Access-Control-Allow-Headers":
+//           "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+//       },
+//     });
+//   }
